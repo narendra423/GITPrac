0314587e21cd2e89e880ca965bb5fb7c322fa6be
narendra423 <narendramedisetti423@gmail.com>
2023-04-11 17:41:13 +0530

    changes going to branch2

--- First.java
+++ First.java
@@ -1,8 +1,8 @@
-public class Prev{
-	private a=10;
-	private b=20;
+public class First{
+	private int a=10;
+	private int  b=20;
 	private String name="branch1";
-	public void static main(String args[]){
+	public static void main(String args[]){
 		System.out.println("sghdsg");
 	}
 }
