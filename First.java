a1018815789e388ef233e8718a389913439e0917
narendra423 <narendramedisetti423@gmail.com>
2023-04-12 10:42:20 +0530

    name branch22 added

--- First.java
+++ First.java
@@ -2,7 +2,7 @@ public class First{
 	private int a=10;
 	private int  b=20;
 	private String name="branch1";
-	private String name="branch2";
+	private String name="branch22";
 	public static void main(String args[]){
 		System.out.println("sghdsg");
 	}
